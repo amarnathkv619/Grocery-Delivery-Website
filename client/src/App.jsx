fe07b90ba6e5234762025508a44159bef32914ee
Amarnath <amaramaru19@gmail.com>
2025-07-06 20:57:47 +0530

    login import error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import Home from "./pages/Home";
 import { Toaster } from "react-hot-toast";
 import Footer from "./components/Footer";
 import { useAppContext } from "./context/AppContext";
-import Login from "./components/LOgin";
+import Login from "./components/Login";
 import AllProducts from "./pages/AllProducts";
 import ProductCategory from "./pages/ProductCategory";
 import ProductDetails from "./pages/ProductDetails";
